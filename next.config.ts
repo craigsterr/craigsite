090fa54be299f8aa76b932282d2c2a6faf18e559
craig <craiglo2@illinois.edu>
2025-06-03 22:05:16 -0500

    changed base path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = "craigsite";
+
 const nextConfig: NextConfig = {
   output: "export",
+  images: { unoptimized: true },
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
